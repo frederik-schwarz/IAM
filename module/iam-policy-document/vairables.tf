dcaa7a2a4307e9a3c33003551fb9f39ae6f4fafe
Frederik <frederik@jens-schwarz.com>
2023-02-14 12:01:00 +1300

    added policy document to the module

---
+++ module/iam-policy-document/vairables.tf
@@ -0,0 +1,22 @@
+variable "create_iam_policy_document" {
+  description = "Create IAM Policy Document."
+  type = bool
+  default = false
+}
+
+variable "iam_policy_document" {
+    description = "Object to create I am policy Document"
+    type = object({
+        statement = list(object({
+            sid = optional(string, null)
+            effect = optional(string, "Allow")
+            actions = optional(list(string), null)
+            resources = optional(list(string), null)
+            principals = optional(list(object({
+                type = string
+                identifiers = list(string)
+            })))
+        }))
+    })
+  
+}
--- variables.tf
+++ variables.tf
@@ -25,6 +25,12 @@ variable "iam_group_config" {
   default = {}
 }
 
+variable "create_iam_policy_document" {
+  description = "Create IAM Policy Document."
+  type = bool
+  default = false
+}
+
 variable "iam_group_membership_config" {
   description = "Object to create IAM Group Membership"
   type = object({
@@ -49,4 +55,21 @@ variable "iam_role_config" {
     tags = optional(map(string), null)
   })
   default = {}
+}
+
+variable "iam_policy_document" {
+    description = "Object to create I am policy Document"
+    type = object({
+        statement = list(object({
+            sid = optional(string, null)
+            effect = optional(string, "Allow")
+            actions = optional(list(string), null)
+            resources = optional(list(string), null)
+            principals = optional(list(object({
+                type = string
+                identifiers = list(string)
+            })))
+        }))
+    })
+  
 }
