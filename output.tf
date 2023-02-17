11cf88dbc4e3de29ac6d20428567e3c11d18bf30
Frederik <frederik@jens-schwarz.com>
2023-02-17 15:39:02 +1300

    fixed naming issue

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "iam_policy_document" {
   depends_on = [
-    module.iam_policy_document[0]
+    module.iam_policy_assume_document[0]
   ]
   description = "The policy document json."
-  value = var.create_iam_policy_document ? module.iam_policy_document[0].json : ""
+  value = var.create_iam_policy_document ? module.iam_policy_assume_document[0].json : ""
 }
