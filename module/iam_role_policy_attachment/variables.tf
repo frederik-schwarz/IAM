b491415ee69854d15a32153a852de2f922fe6513
Frederik <frederik@jens-schwarz.com>
2023-02-17 16:22:04 +1300

    attachment

---
+++ module/iam_role_policy_attachment/variables.tf
@@ -0,0 +1,13 @@
+variable "create_iam_role_policy_attachment" {
+  description = "Value to create new IAM attachment."
+  default = false
+  type    = bool
+}
+
+variable "iam_role_policy_attachment" {
+  description = "Object to set attachment values."
+  type = object({
+    role = optional(string, null)
+    policy_arn = optional(string, null)
+  })
+}
